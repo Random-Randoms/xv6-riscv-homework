406296649b0aec9b65f4e93498c47741f72e1747
Random-Randoms <daniil.savinov.05@gmail.com>
2025-02-23 14:18:31 +0300

    task 2 plus 1a

---
+++ user/add.c
@@ -0,0 +1,61 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+void skip() {
+  char cur;
+  while (read(0, &cur, 1) == 1)
+    if (cur == '\n' || cur == '\r')
+      break;
+}
+
+
+
+int
+main(int argc, char *argv[])
+{
+  char format_msg[] = "format error: two space-separated numbers expected\n";
+  int bfsize = 20;
+  char* bf = malloc(bfsize * sizeof(char));
+  char* snd;
+  char cur;
+  int i, l, r, res;
+  int status = 0;
+
+  for (i = 0; i < bfsize; ++i) {
+    if (read(0, &cur, 1) < 1 || cur == '\r' || cur == '\n' || cur == '\0') {
+      break;
+    }
+
+    bf[i] = cur;
+
+    if (cur == ' ') {
+      if (status == 0)
+        status = 1, snd = bf + i + 1;
+      else
+        status = -1;
+      continue;
+    }
+
+    if (cur > '9' || cur < '0') {
+      status = -1;
+      continue;
+    }
+  }
+  bf[i] = '\0';
+
+  if (status == 1) {
+
+  l = atoi(bf);
+  r = atoi(snd);
+
+  add(l, r, &res);
+
+  printf("%u\n", res);
+  }
+  else {
+    printf("%s", format_msg);
+  }
+
+  printf("|%s|\n", bf);
+}
